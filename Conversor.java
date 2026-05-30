000331dddea62e442d02cb0b7ada6b0001b22915
Thie <thie.nadjarian@gmail.com>
2026-05-30 15:14:57 -0300

    feat: implemented Temperature Conversion

--- Conversor.java
+++ Conversor.java
@@ -1,12 +1,28 @@
 public class Conversor {
 
+    // KM -> Metros
     public double kmParaMetros(double km) {
         double m = km * 1000;
         return m;
     }
 
+    // Metros -> KM
     public double metroParaKm(double m){
         double km = kmParaMetros(m);
         return km;
     }
+
+    // Celsius -> Farenheint
+    public double celsiusParaFahr(double c){
+        double fahr = c * 9 / 5  + 32;
+        return fahr;
+    }
+
+    // Fahrenheit -> Celsius
+    public double fahrParaCelsius(double fahr){
+        double cels = (fahr - 32) * 5 / 9;
+        return cels;
+    }
+
+    
 }
